48177cdc66ff63f14dc9efbc7a9c311fbd61296b
tedzchu <tedchu4@gmail.com>
2020-01-31 12:50:51 -0800

    React classes example. Gross and needs more hooks

--- part-4-get-array-react/src/App.js
+++ part-4-get-array-react/src/App.js
@@ -1,24 +1,85 @@
+import React, { Component } from 'react';
+import moment from 'moment';
 import './App.css';
+import LineChart from './LineChart';
+import ToolTip from './ToolTip';
+import InfoBox from './InfoBox';
 
-import { Line, LineChart } from 'recharts';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fetchingData: true,
+      data: null,
+      hoverLoc: null,
+      activePoint: null
+    }
+  }
+  handleChartHover(hoverLoc, activePoint){
+    this.setState({
+      hoverLoc: hoverLoc,
+      activePoint: activePoint
+    })
+  }
+  componentDidMount(){
+    const getData = () => {
+      const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=[YOUR_KEY_HERE]';
 
-import React from 'react';
+      fetch(url).then( r => r.json())
+        .then((stockData) => {
+          const sortedData = [];
+          let count = 29;
+          for (let date in stockData['Time Series (Daily)']){
+            console.log(date, count, stockData['Time Series (Daily)'][date]['4. close'])
+            sortedData.unshift({
+              d: moment(date).format('MMM DD'),
+              p: stockData['Time Series (Daily)'][date]['4. close'].toLocaleString('us-EN',{ style: 'currency', currency: 'USD' }),
+              x: count, //previous days
+              y: stockData['Time Series (Daily)'][date]['4. close'] // numerical price
+            });
+            count--;
+            if (count < 0) {
+              break;
+            }
+          }
+          this.setState({
+            data: sortedData,
+            fetchingData: false
+          })
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+    getData();
+  }
+  render() {
+    return (
 
-function App() {
-  const data = [
-    { name: 'Page A', uv: 400, pv: 2400, amt: 2400 },
-    { name: 'Page B', uv: 400, pv: 300, amt: 300 },
-    { name: 'Page c', uv: 300, pv: 100, amt: 100 },
-    { name: 'Page c', uv: 300, pv: 100, amt: 200 }
-  ];
-
-  return (
-    <div className='App'>
-      <LineChart width={400} height={400} data={data}>
-        <Line type='monotone' dataKey='uv' stroke='#8884d8' />
-      </LineChart>
-    </div>
-  );
+      <div className='container'>
+        <div className='row'>
+          <h1>30 (Trading) Day TSLA Price Chart</h1>
+        </div>
+        <div className='row'>
+          { !this.state.fetchingData ?
+          <InfoBox data={this.state.data} />
+          : null }
+        </div>
+        <div className='row'>
+          <div className='popup'>
+            {this.state.hoverLoc ? <ToolTip hoverLoc={this.state.hoverLoc} activePoint={this.state.activePoint}/> : null}
+          </div>
+        </div>
+        <div className='row'>
+          <div className='chart'>
+            { !this.state.fetchingData ?
+              <LineChart data={this.state.data} onChartHover={ (a,b) => this.handleChartHover(a,b) }/>
+              : null }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
