44b9b693d9178a5f99ab052529fd70b89efededd
tedzchu <tedchu4@gmail.com>
2020-02-01 03:40:51 -0800

    Add auth error handling for post

--- part-2-post-gist/script.js
+++ part-2-post-gist/script.js
@@ -5,14 +5,21 @@ function createGist(opts) {
     body: JSON.stringify(opts),
     headers: new Headers({
       'Content-Type': 'application/json',
-      Authorization: 'token [GITHUB_TOKEN]'
+      Authorization: 'token [YOUR_TOKEN_HERE]'
     })
   })
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
-      console.log('Created Gist:', data.html_url);
+      if (data.html_url) {
+        console.log('Created Gist:', data.html_url);
+        document.querySelector('span').innerHTML =
+          '<a href="' + data.html_url + '">View Your New Gist Here!</a>';
+      } else {
+        document.querySelector('span').innerHTML =
+          'There was an error creating your Gist. Please check your authentication headers.';
+      }
     });
 }
 
