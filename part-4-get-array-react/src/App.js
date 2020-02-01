5148ea11bd6cee62b7bdcf8b650f23ed45c07670
tedzchu <tedchu4@gmail.com>
2020-02-01 00:23:09 -0800

    Formatting and package updates

--- part-4-get-array-react/src/App.js
+++ part-4-get-array-react/src/App.js
@@ -13,27 +13,35 @@ class App extends Component {
       data: null,
       hoverLoc: null,
       activePoint: null
-    }
+    };
   }
-  handleChartHover(hoverLoc, activePoint){
+  handleChartHover(hoverLoc, activePoint) {
     this.setState({
       hoverLoc: hoverLoc,
       activePoint: activePoint
-    })
+    });
   }
-  componentDidMount(){
+  componentDidMount() {
     const getData = () => {
-      const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=[YOUR_KEY_HERE]';
+      const url =
+        'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=[YOUR_KEY_HERE]';
 
-      fetch(url).then( r => r.json())
-        .then((stockData) => {
+      fetch(url)
+        .then(r => r.json())
+        .then(stockData => {
           const sortedData = [];
           let count = 29;
-          for (let date in stockData['Time Series (Daily)']){
-            console.log(date, count, stockData['Time Series (Daily)'][date]['4. close'])
+          for (let date in stockData['Time Series (Daily)']) {
+            console.log(
+              date,
+              count,
+              stockData['Time Series (Daily)'][date]['4. close']
+            );
             sortedData.unshift({
               d: moment(date).format('MMM DD'),
-              p: stockData['Time Series (Daily)'][date]['4. close'].toLocaleString('us-EN',{ style: 'currency', currency: 'USD' }),
+              p: stockData['Time Series (Daily)'][date][
+                '4. close'
+              ].toLocaleString('us-EN', { style: 'currency', currency: 'USD' }),
               x: count, //previous days
               y: stockData['Time Series (Daily)'][date]['4. close'] // numerical price
             });
@@ -45,36 +53,41 @@ class App extends Component {
           this.setState({
             data: sortedData,
             fetchingData: false
-          })
+          });
         })
-        .catch((e) => {
+        .catch(e => {
           console.log(e);
         });
-    }
+    };
     getData();
   }
   render() {
     return (
-
       <div className='container'>
         <div className='row'>
           <h1>30 (Trading) Day TSLA Price Chart</h1>
         </div>
         <div className='row'>
-          { !this.state.fetchingData ?
-          <InfoBox data={this.state.data} />
-          : null }
+          {!this.state.fetchingData ? <InfoBox data={this.state.data} /> : null}
         </div>
         <div className='row'>
           <div className='popup'>
-            {this.state.hoverLoc ? <ToolTip hoverLoc={this.state.hoverLoc} activePoint={this.state.activePoint}/> : null}
+            {this.state.hoverLoc ? (
+              <ToolTip
+                hoverLoc={this.state.hoverLoc}
+                activePoint={this.state.activePoint}
+              />
+            ) : null}
           </div>
         </div>
         <div className='row'>
           <div className='chart'>
-            { !this.state.fetchingData ?
-              <LineChart data={this.state.data} onChartHover={ (a,b) => this.handleChartHover(a,b) }/>
-              : null }
+            {!this.state.fetchingData ? (
+              <LineChart
+                data={this.state.data}
+                onChartHover={(a, b) => this.handleChartHover(a, b)}
+              />
+            ) : null}
           </div>
         </div>
       </div>
