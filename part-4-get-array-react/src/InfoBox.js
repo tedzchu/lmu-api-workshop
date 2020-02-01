ceefe5f925dac4f3782edbfaa11e4fd2af98d12e
tedzchu <tedchu4@gmail.com>
2020-02-01 00:47:19 -0800

    Fixes to dollar formatting

--- part-4-get-array-react/src/InfoBox.js
+++ part-4-get-array-react/src/InfoBox.js
@@ -10,56 +10,72 @@ class InfoBox extends Component {
       monthChangeD: null,
       monthChangeP: null,
       updatedAt: null
-    }
+    };
   }
-  componentDidMount(){
+  componentDidMount() {
     this.getData = () => {
-      const {data} = this.props;
-      const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=[YOUR_KEY_HERE]';
+      const { data } = this.props;
+      const url =
+        'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=[YOUR_KEY_HERE]';
 
-      fetch(url).then(r => r.json())
-        .then((stockData) => {
-          const price = stockData['Time Series (Daily)'][Object.keys(stockData['Time Series (Daily)'])[0]]['4. close'];
+      fetch(url)
+        .then(r => r.json())
+        .then(stockData => {
+          const price =
+            stockData['Time Series (Daily)'][
+              Object.keys(stockData['Time Series (Daily)'])[0]
+            ]['4. close'];
           const change = price - data[0].y;
-          const changeP = (price - data[0].y) / data[0].y * 100;
-
+          const changeP = ((price - data[0].y) / data[0].y) * 100;
           this.setState({
-            currentPrice: stockData['Time Series (Daily)'][Object.keys(stockData['Time Series (Daily)'])[0]]['4. close'],
-            monthChangeD: change.toLocaleString('us-EN',{ style: 'currency', currency: 'USD' }),
+            currentPrice: Number(price).toLocaleString('us-EN', {
+              style: 'currency',
+              currency: 'USD'
+            }),
+            monthChangeD: change.toLocaleString('us-EN', {
+              style: 'currency',
+              currency: 'USD'
+            }),
             monthChangeP: changeP.toFixed(2) + '%',
             updatedAt: new moment()
-          })
+          });
         })
-        .catch((e) => {
+        .catch(e => {
           console.log(e);
         });
-    }
+    };
     this.getData();
     this.refresh = setInterval(() => this.getData(), 90000);
   }
-  componentWillUnmount(){
+  componentWillUnmount() {
     clearInterval(this.refresh);
   }
-  render(){
+  render() {
     return (
-      <div id="data-container">
-        { this.state.currentPrice ?
-          <div id="left" className='box'>
-            <div className="heading">{this.state.currentPrice.toLocaleString('us-EN',{ style: 'currency', currency: 'USD' })}</div>
-            <div className="subtext">{'Updated ' + moment(this.state.updatedAt ).fromNow()}</div>
+      <div id='data-container'>
+        {this.state.currentPrice ? (
+          <div id='left' className='box'>
+            <div className='heading'>
+              {this.state.currentPrice.toLocaleString('us-EN', {
+                style: 'currency',
+                currency: 'USD'
+              })}
+            </div>
+            <div className='subtext'>
+              {'Updated ' + moment(this.state.updatedAt).fromNow()}
+            </div>
           </div>
-        : null}
-        { this.state.currentPrice ?
-          <div id="middle" className='box'>
-            <div className="heading">{this.state.monthChangeD}</div>
-            <div className="subtext">Change Since Last Month (USD)</div>
+        ) : null}
+        {this.state.currentPrice ? (
+          <div id='middle' className='box'>
+            <div className='heading'>{this.state.monthChangeD}</div>
+            <div className='subtext'>Change Since Last Month (USD)</div>
           </div>
-        : null}
-          <div id="right" className='box'>
-            <div className="heading">{this.state.monthChangeP}</div>
-            <div className="subtext">Change Since Last Month (%)</div>
-          </div>
-
+        ) : null}
+        <div id='right' className='box'>
+          <div className='heading'>{this.state.monthChangeP}</div>
+          <div className='subtext'>Change Since Last Month (%)</div>
+        </div>
       </div>
     );
   }
